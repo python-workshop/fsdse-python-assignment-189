3c00cded84894f4dc0813797addfd92c2c93b6d9
LENOVO-PC\Sourabh Ail <sourabh1419@gmail.com>
2017-05-09 15:15:03 +0530

    Add Code

---
+++ build.py
@@ -0,0 +1,12 @@
+#Generate a list of primes
+def is_prime(num):
+    if num == 0 or num == 1:
+        return False
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    else:
+        return True
+
+x = list(filter(is_prime, range(1, 30)))
+print(x)
